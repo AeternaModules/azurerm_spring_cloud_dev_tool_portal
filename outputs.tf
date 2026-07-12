a7c2b1b35f4dd8038e0a396986c4217cc9094c46
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "spring_cloud_dev_tool_portals" {
-  description = "All spring_cloud_dev_tool_portal resources"
-  value       = azurerm_spring_cloud_dev_tool_portal.spring_cloud_dev_tool_portals
-}
 output "spring_cloud_dev_tool_portals_application_accelerator_enabled" {
-  description = "List of application_accelerator_enabled values across all spring_cloud_dev_tool_portals"
-  value       = [for k, v in azurerm_spring_cloud_dev_tool_portal.spring_cloud_dev_tool_portals : v.application_accelerator_enabled]
+  description = "Map of application_accelerator_enabled values across all spring_cloud_dev_tool_portals, keyed the same as var.spring_cloud_dev_tool_portals"
+  value       = { for k, v in azurerm_spring_cloud_dev_tool_portal.spring_cloud_dev_tool_portals : k => v.application_accelerator_enabled }
 }
 output "spring_cloud_dev_tool_portals_application_live_view_enabled" {
-  description = "List of application_live_view_enabled values across all spring_cloud_dev_tool_portals"
-  value       = [for k, v in azurerm_spring_cloud_dev_tool_portal.spring_cloud_dev_tool_portals : v.application_live_view_enabled]
+  description = "Map of application_live_view_enabled values across all spring_cloud_dev_tool_portals, keyed the same as var.spring_cloud_dev_tool_portals"
+  value       = { for k, v in azurerm_spring_cloud_dev_tool_portal.spring_cloud_dev_tool_portals : k => v.application_live_view_enabled }
 }
 output "spring_cloud_dev_tool_portals_name" {
-  description = "List of name values across all spring_cloud_dev_tool_portals"
-  value       = [for k, v in azurerm_spring_cloud_dev_tool_portal.spring_cloud_dev_tool_portals : v.name]
+  description = "Map of name values across all spring_cloud_dev_tool_portals, keyed the same as var.spring_cloud_dev_tool_portals"
+  value       = { for k, v in azurerm_spring_cloud_dev_tool_portal.spring_cloud_dev_tool_portals : k => v.name }
 }
 output "spring_cloud_dev_tool_portals_public_network_access_enabled" {
-  description = "List of public_network_access_enabled values across all spring_cloud_dev_tool_portals"
-  value       = [for k, v in azurerm_spring_cloud_dev_tool_portal.spring_cloud_dev_tool_portals : v.public_network_access_enabled]
+  description = "Map of public_network_access_enabled values across all spring_cloud_dev_tool_portals, keyed the same as var.spring_cloud_dev_tool_portals"
+  value       = { for k, v in azurerm_spring_cloud_dev_tool_portal.spring_cloud_dev_tool_portals : k => v.public_network_access_enabled }
 }
 output "spring_cloud_dev_tool_portals_spring_cloud_service_id" {
-  description = "List of spring_cloud_service_id values across all spring_cloud_dev_tool_portals"
-  value       = [for k, v in azurerm_spring_cloud_dev_tool_portal.spring_cloud_dev_tool_portals : v.spring_cloud_service_id]
+  description = "Map of spring_cloud_service_id values across all spring_cloud_dev_tool_portals, keyed the same as var.spring_cloud_dev_tool_portals"
+  value       = { for k, v in azurerm_spring_cloud_dev_tool_portal.spring_cloud_dev_tool_portals : k => v.spring_cloud_service_id }
 }
 output "spring_cloud_dev_tool_portals_sso" {
-  description = "List of sso values across all spring_cloud_dev_tool_portals"
-  value       = [for k, v in azurerm_spring_cloud_dev_tool_portal.spring_cloud_dev_tool_portals : v.sso]
+  description = "Map of sso values across all spring_cloud_dev_tool_portals, keyed the same as var.spring_cloud_dev_tool_portals"
+  value       = { for k, v in azurerm_spring_cloud_dev_tool_portal.spring_cloud_dev_tool_portals : k => v.sso }
 }
 
